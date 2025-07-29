72ec13d0cfb7a957937f5b373ba14ff8083c5bd0
Richelieu Valentin <richelieuvalentin@gmail.com>
2025-07-29 20:46:25 -0300

    Scripts utilizando listas, sets, tuplas e dicionários

--- hello.py
+++ hello.py
@@ -16,22 +16,19 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Richelieu Valentin"
 __license__ = "Unlicense"
 
 import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde"
-    
-print(msg)
+msg = {
+    "en_US": "Hello, World",
+    "pt_BR": "Olá, Mundo",
+    "it_IT": "Ciao, Mondo",
+    "fr_FR": "Bonjour, Monde",
+}
+   
+print(msg[current_language])
